4c06d3d71e4d848d2ff3f9b640c1d2593b777ea8
Arcriles <30470009+arcriles@users.noreply.github.com>
2023-10-24 12:18:58 +0700

    Update Default.aspx.vb

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,5 +1,20 @@
+Imports System.Data.SqlClient
 
 Partial Class _Default
     Inherits System.Web.UI.Page
 
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            Dim connectionString As String = ConfigurationManager.ConnectionStrings("MyConnection").ConnectionString
+            Using conn As New SqlConnection(connectionString)
+                conn.Open()
+                Using cmd As New SqlCommand("SELECT ID, Username, Password, groupId, RealName, Jabatan FROM Users", conn)
+                    Using reader As SqlDataReader = cmd.ExecuteReader()
+                        UsersGridView.DataSource = reader
+                        UsersGridView.DataBind()
+                    End Using
+                End Using
+            End Using
+        End If
+    End Sub
 End Class
