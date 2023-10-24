90f0440cb8eaa083576f46dc5cbbbeff44893e13
Arcriles <30470009+arcriles@users.noreply.github.com>
2023-10-24 13:05:40 +0700

    Update Default.aspx.vb

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,24 +1,22 @@
-Imports System.Data
 Imports System.Data.SqlClient
 
-Partial Class Default
+Partial Class _Default
     Inherits System.Web.UI.Page
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
         If Not IsPostBack Then
-            Dim connectionString As String = "Data Source=YourServer;Initial Catalog=HRD;User Id=YourUsername;Password=YourPassword;"
-            Dim conn As New SqlConnection(connectionString)
-            Dim cmd As New SqlCommand("SELECT ID, Username, Password, groupId, RealName, Jabatan FROM Users", conn)
-            Dim adapter As New SqlDataAdapter(cmd)
-            Dim ds As New DataSet()
-
-            conn.Open()
-            adapter.Fill(ds, "Users")
-            conn.Close()
-
-            UsersGridView.DataSource = ds.Tables("Users")
-            UsersGridView.DataBind()
+            Dim connectionString As String = ConfigurationManager.ConnectionStrings("QL_CONN").ConnectionString
+            Using conn As New SqlConnection(connectionString)
+                conn.Open()
+                Using cmd As New SqlCommand("SELECT ID, Username, Password, groupId, RealName, Jabatan FROM Users", conn)
+                    Using reader As SqlDataReader = cmd.ExecuteReader()
+                        UsersGridView.DataSource = reader
+                        UsersGridView.DataBind()
+                    End Using
+                End Using
+            End Using
         End If
     End Sub
 End Class
+
 
